53501f077b58bc654b1e302b4010b84c2880d2c2
Sabarish <sabarish90@live.com>
2024-08-15 13:25:33 +0530

    files replicator cut 1

--- cdf_fabric_replicator/file.py
+++ cdf_fabric_replicator/file.py
@@ -11,15 +11,21 @@ import pyarrow as pa
 from cdf_fabric_replicator import __version__
 from cdf_fabric_replicator.config import Config
 from cdf_fabric_replicator.metrics import Metrics
+from datetime import datetime, timezone
 from cognite.client.data_classes import FileMetadataList
 from datetime import datetime
-
+import random
+import string
+import base64
+import os
+from datetime import datetime
+from azure.storage.filedatalake import DataLakeServiceClient
 
 class FileReplicator(Extractor):
     def __init__(self, metrics: Metrics, stop_event: CancellationToken) -> None:
         super().__init__(
             name="cdf_fabric_replicator_file",
-            description="CDF Fabric Replicator",
+            description="CDF Fabric File Replicator",
             config_class=Config,
             metrics=metrics,
             use_default_state_store=False,
@@ -29,6 +35,9 @@ class FileReplicator(Extractor):
         self.azure_credential = DefaultAzureCredential()
         self.stop_event = stop_event
         self.logger = logging.getLogger(self.name)
+        self.file_store_account_name = "onelake"
+        self.file_store_workspace_name = "TestWorkspaceForCDF"
+        self.file_store_lakehouse_files_path = "/LH_CDF.Lakehouse/Files/"
 
     def run(self) -> None:
         self.logger.info("Run Called for File Extractor...")
@@ -56,6 +65,15 @@ class FileReplicator(Extractor):
         self.logger.info("Stop event set. Exiting...")
 
     def process_files(self) -> None:
+
+        testRunId = str(int(time.time()))
+        # Create directory
+        directory = f"./runs/{testRunId}"
+        os.makedirs(directory, exist_ok=True)
+        
+        # self.cognite_client.files.download(directory=f"./runs/{testRunId}", id=[1314827462876424])
+        # exit()
+
         for mime_type in self.config.file.mime_types:
             state_id = f"{mime_type}_state"
             last_updated_time = self.get_state(state_id)
@@ -76,14 +94,71 @@ class FileReplicator(Extractor):
             while feedRows:
                 files = self.cognite_client.files.list(
                     mime_type = mime_type,
+                    data_set_ids=[6570415114656834],
+                    limit=None
                 )
+
                 if len(files) > 0:
                     try:
+                        token = self.azure_credential.get_token("https://storage.azure.com/.default")
+                        
+
                         self.write_rows_to_lakehouse_table(
                             files, abfss_path=self.config.file.lakehouse_abfss_path_files
                         )
                         last_row = files[-1]
                         self.set_state(state_id, last_row.last_updated_time)
+
+                        print("Downloading files now")
+                        file_ids = [file.id for file in files]
+                        self.cognite_client.files.download(directory=f"./runs/{testRunId}", id=file_ids)
+                        
+                        files_data_dict = []
+                        for file in files:        
+                            self.logger.debug(f"Downloading file {file.id}, name is {file.name}")                            
+                            # Path to the image file
+                            image_path = f"./runs/{testRunId}/{file.name}"
+
+                            # Open the image file in binary mode
+                            with open(image_path, "rb") as image_file:
+                                # Read the image file and encode it to base64
+                                encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
+                            
+                            file_data_dict = dict()
+                            file_data_dict["name"] = file.name
+                            file_data_dict["key"] = file.name
+                            file_data_dict["id"] = file.id
+                            file_data_dict["content"] = encoded_string
+                            html_content = f'<img src="data:{mime_type};base64,{encoded_string}" alt="Image">'
+                            file_data_dict["imgcontent"] = html_content
+
+                            files_data_dict.append(file_data_dict)
+                            
+                        abfss_path = f"abfss://35100caa-1ac5-4145-bc50-81cfa7acc0a6@onelake.dfs.fabric.microsoft.com/afa834a7-de7c-42dc-8f06-c61d07998fd9/Tables/FileHxData_t"
+                        token = self.azure_credential.get_token("https://storage.azure.com/.default")
+
+
+                        # Your ADLS Gen2 account URL (use https for SDK access)
+                        account_url = "https://35100caa-1ac5-4145-bc50-81cfa7acc0a6.dfs.core.windows.net"
+
+                        if len(files_data_dict) > 0:
+                            self.logger.info(f"Writing {len(files_data_dict)} FileData to '{abfss_path}' table...")
+                            data = pa.Table.from_pylist(files_data_dict)
+                            storage_options = {
+                                "bearer_token": token.token,
+                                "use_fabric_endpoint": "true",
+                            }
+
+                            try:
+                                self.write_or_merge_to_lakehouse_table(
+                                    abfss_path, storage_options, data
+                                )
+                            except DeltaError as e:
+                                self.logger.error(f"Error writing rows to lakehouse tables: {e}")
+                                raise e
+
+                            self.logger.info("Done saving file metadata.")
+                            
                     except DeltaError as e:
                         self.logger.error(
                             f"Error writing file rows to lakehouse tables: {e}"
@@ -91,6 +166,8 @@ class FileReplicator(Extractor):
                         raise e
                 else:
                     feedRows = False
+                feedRows = False
+            
 
     def get_state(self, state_key: str) -> int | None:
         state = self.state_store.get_state(external_id=state_key)
@@ -110,8 +187,58 @@ class FileReplicator(Extractor):
         token = self.azure_credential.get_token("https://storage.azure.com/.default")
 
         files_dict = []
+        self.logger.debug(f"------------------")
+        self.logger.debug(f"{files}")
+        self.logger.debug(f"------------------")
+        runId = str(int(time.time()))
+        
+        account_url = f"https://{self.file_store_account_name}.dfs.fabric.microsoft.com"
+        token_credential = DefaultAzureCredential()
+        service_client = DataLakeServiceClient(account_url, credential=token_credential)
+        file_system_client = service_client.get_file_system_client(self.file_store_workspace_name)
+        directory_client = file_system_client.create_directory(f'/{self.file_store_lakehouse_files_path}/images/robot-garden')
+        directory = f"./runs/{runId}"
+        os.makedirs(directory, exist_ok=True)
+
+        file_ids = [file.id for file in files]
+        self.cognite_client.files.download(directory=f"./runs/{runId}", id=file_ids)
+
         for file in files:
+            
+            vm_file_path = f"./runs/{runId}/{file.name}"
+            file_extension = os.path.splitext(file.name)[1]
+            onelake_filename = str(file.id) + file_extension
+            file_client = directory_client.get_file_client(onelake_filename)
+
+            with open(file=vm_file_path, mode="rb") as data:
+                file_client.upload_data(data, overwrite=True)
+
+            filetimestamp="0"
+            
+            if file.metadata is not None:
+                if "asset_external_id" in file.metadata:
+                    asset_external_id = file.metadata["asset_external_id"]
+                else:
+                    asset_external_id = "NA"
+                    
+                if "timestamp" in file.metadata:
+                    filetimestamp = str(file.metadata["timestamp"])
+                else:
+                    filetimestamp = "0"
+            else:
+                asset_external_id = "NA"
+
             file_dict = dict()
+            file_dict["runId"] = runId
+            file_dict["lh_path"] = "Files/images/robot-garden" + "/" + onelake_filename
+            file_dict["asset_external_id"] = asset_external_id
+            file_dict["metadata_timestamp"] = str(filetimestamp)
+            
+            ts = int(filetimestamp)  # Assuming `file.timestamp` is the timestamp string
+            datetime_utc= datetime.fromtimestamp(ts / 1000, tz=timezone.utc)
+            file_dict["metadata_timestamp_str"] = datetime_utc
+            file_dict["uri"] = "https://filepath"
+            file_dict["key"] = str(file.id)
             file_dict["external_id"] = str(file.external_id)
             file_dict["name"] = str(file.name)
             file_dict["source"] = str(file.source)
@@ -131,6 +258,7 @@ class FileReplicator(Extractor):
             file_dict["created_time"] = str(file.created_time)
             file_dict["last_updated_time"] = str(file.last_updated_time)
             files_dict.append(file_dict)
+        
         if len(files_dict) > 0:
             self.logger.info(f"Writing {len(files)} rows to '{abfss_path}' table...")
             data = pa.Table.from_pylist(files_dict)
@@ -147,7 +275,8 @@ class FileReplicator(Extractor):
                 self.logger.error(f"Error writing rows to lakehouse tables: {e}")
                 raise e
 
-            self.logger.info("Done.")
+            self.logger.info("Done saving file metadata.")
+        
 
     def write_or_merge_to_lakehouse_table(
         self, abfss_path: str, storage_options: Dict[str, str], data: pa.Table
